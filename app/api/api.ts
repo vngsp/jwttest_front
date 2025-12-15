84c46170e0f31c2ed5aa0b5b184654fdd0ee4217
vngsp <viniciuspenhadev@gmail.com>
2025-12-15 16:49:12 -0300

    Refactor: Use NEXT_PUBLIC_API_URL environment variable for production

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const req = axios.create({
-    baseURL: 'http://localhost:1000'
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1000'
 });
 
 req.interceptors.request.use((config) => {
